ef801539733b212f690022c58034455a71de0c4a
David Shere <david.e.shere@gmail.com>
2015-08-07 18:50:02 -0700

    more changes

--- src/Iris-Frames.hs
+++ src/Iris-Frames.hs
@@ -9,30 +9,34 @@ import Unsafe.Coerce
 import Data.List.Split
 import Text.Printf
 
+myFilter (r:g:b:[]) = r `div` 2 + g `div` (if b /= 0 then b else 1)
+
 main = do
     args <- getArgs
     ri <- readImage $ args !! 0
     case ri of
         Left err -> hPutStrLn stderr err
-        Right di -> makeFrames di $ args !! 1
+        Right di -> makeFrames myFilter di $ args !! 1
 
-makeFrames (ImageRGB8 i@(Image w h d)) outDir = do
+
+makeFrames f (ImageRGB8 i@(Image w h d)) outDir = do
     let ld = (map fromIntegral $ V.toList d) 
     let vd = zipWith (\a b -> (a,b))
-            (map sum $ chunksOf 3 ld)
+            (map f $ chunksOf 3 ld)
             (chunksOf 3 ld)
-    let values = [ map (\(a,b) -> if a == i then b++[255] else [0,0,0,0]) vd | i <- [0..255+255+255] ]
+    let values = [ map (\(a,b) -> if a == i then b++[255] else [0,0,0,0]) vd | i <- [0..(maxKey vd)]]
     let frames = map (Image w h . V.fromList . map fromIntegral) $ map concat values
     putStrLn $ show . length $ vd
     saveFrames (0::Integer) frames
   where
-    saveFrames n (f:fs) = do putStrLn $ "Frame Left " ++ (show $ length fs + 1)
+    saveFrames n (f:fs) = do putStrLn $ "Frames Left " ++ (show $ length fs )
                              savePngImage outFile $ ImageRGBA8 f
                              saveFrames (n+1) fs
         where outFile :: String
               outFile = fileNameOf outDir n
     saveFrames _ []   = return ()
-makeFrames _ _ = hPutStrLn stderr "unsupported file format"
+    maxKey = foldr (\(a,_) b -> max a b) 0
+makeFrames _ _ _ = hPutStrLn stderr "unsupported file format"
 
 fileNameOf :: (Integral a, PrintfArg a) => String -> a -> String
 fileNameOf dir n = printf "%s/%09d.png" dir n
